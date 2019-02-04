af30420b98f64583a3772b4bf6a4bb039a99f2b1
maxkvant <max.kvant@gmail.com>
2019-02-05 02:47:16 +0300

    first version

--- camtrack/corners.py
+++ camtrack/corners.py
@@ -37,13 +37,49 @@ class _CornerStorageBuilder:
 def _build_impl(frame_sequence: pims.FramesSequence,
                 builder: _CornerStorageBuilder) -> None:
     image_0 = frame_sequence[0]
+    size_default = 40
+
+    def get_features(image):
+        return cv2.goodFeaturesToTrack(image, maxCorners=100, qualityLevel=0.2, minDistance=size_default)
+
+    points = get_features(image_0)
+    n = len(points)
+    ids = np.arange(n)
+
     corners = FrameCorners(
-        np.array([0]),
-        np.array([[0, 0]]),
-        np.array([55])
+        ids=ids,
+        points=np.array(points),
+        sizes=np.array([size_default] * len(points))
     )
+
     builder.set_corners_at_frame(0, corners)
+
+    lk_params = dict(winSize=(15, 15),
+                     maxLevel=2,
+                     criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+
     for frame, image_1 in enumerate(frame_sequence[1:], 1):
+        cur_points, status, err = cv2.calcOpticalFlowPyrLK((image_0 * 255.0).astype(np.uint8), (image_1 * 255.0).astype(np.uint8), points, None, **lk_params)
+        status = status[:, 0]
+        print((status.shape, cur_points.shape))
+
+        cur_points = cur_points[status == 1, :, :]
+        cur_ids = ids[status == 1]
+
+        new_points = get_features(image_0)
+        new_ids = n + np.arange(len(new_points))
+        n += len(new_points)
+
+        ids = np.concatenate((cur_ids, new_ids))
+        points = np.concatenate((cur_points, new_points))
+
+        corners = FrameCorners(
+            ids=ids,
+            points=np.array(points),
+            sizes=np.array([size_default] * len(points))
+        )
+
         builder.set_corners_at_frame(frame, corners)
         image_0 = image_1
 
